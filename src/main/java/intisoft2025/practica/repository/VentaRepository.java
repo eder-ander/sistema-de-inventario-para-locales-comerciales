108c03613b66b607dc8955c5ad3b043dd39ef0df
ander <ederander123@gmail.com>
2026-03-18 12:22:36 -0500

    feat(repository) created interface ventarepository with methods crud predefined

--- src/main/java/intisoft2025/practica/repository/VentaRepository.java
+++ src/main/java/intisoft2025/practica/repository/VentaRepository.java
@@ -1,4 +1,7 @@
 package intisoft2025.practica.repository;
 
-public class VentaRepository {
+import intisoft2025.practica.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
 }
