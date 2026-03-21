a3ebd8913c660cbd8449e9b504933d42e4843bf9
ander <ederander123@gmail.com>
2026-03-21 11:04:15 -0500

    feat(repository) delete because was added method helper in venta.class

--- src/main/java/intisoft2025/practica/repository/DetalleVentaRepository.java
+++
@@ -1,8 +0,0 @@
-package intisoft2025.practica.repository;
-
-import intisoft2025.practica.model.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-
-}
