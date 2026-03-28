5ac08d7dbba183ae165c17d4984b6e44f5a69f94
ander <ederander123@gmail.com>
2026-03-28 13:35:26 -0500

    feat(dto) created dto for get producto

---
+++ src/main/java/intisoft2025/practica/dto/RequestProductoDto.java
@@ -0,0 +1,44 @@
+package intisoft2025.practica.dto;
+
+public class RequestProductoDto {
+    private Long id;
+    private String nombre;
+    private Integer precio;
+    private Integer cantidad;
+
+    public RequestProductoDto(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
